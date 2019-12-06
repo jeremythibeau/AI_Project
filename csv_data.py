f2359e31f433645f29c6f27b64f914c0ec6dd236
coolbians <ianmiranda910@gmail.com>
2019-12-05 21:14:47 -0500

    adding read_data func

--- csv_data.py
+++ csv_data.py
@@ -9,6 +9,12 @@ def overwrite_data(filepath):
         writer.writeheader()
 
 
+def read_data(filepath):
+    with open(filepath, "r", newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            print(row)
+
 def get_length(filepath):
     with open("data.csv", "r", newline='') as csvfile:
         reader = csv.reader(csvfile)
@@ -36,11 +42,12 @@ def append_data(filepath, name, player_score, comp_score, comp_has_ace, decision
 overwrite_data("data.csv")
 append_data("data.csv", "johnny", 21, 19, 1, 0)
 append_data("data.csv", "mary", 20, 21, 0, 0)
-append_data("data.csv", "mary", 20, 21, 0, 0)
-append_data("data.csv", "mary", 20, 21, 0, 0)
-append_data("data.csv", "mary", 20, 21, 0, 0)
-append_data("data.csv", "mary", 20, 21, 0, 0)
-append_data("data.csv", "mary", 20, 21, 0, 0)
+append_data("data.csv", "mar", 20, 22, 0, 0)
+append_data("data.csv", "may", 20, 23, 0, 0)
+append_data("data.csv", "mry", 20, 24, 0, 0)
+append_data("data.csv", "ary", 20, 25, 0, 0)
+append_data("data.csv", "ma", 20, 26, 0, 0)
+read_data("data.csv")
 
 
 

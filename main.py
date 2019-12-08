6a2b466aa881ba9f33ddf6eaed3433ce537eda6a
sharpmanjt <thibeau.jeremy@gmail.com>
2019-12-07 23:29:48 -0500

    pushing all functionality

--- main.py
+++ main.py
@@ -12,6 +12,60 @@ from Card import Card
 def clearScreen():
     print("\n" * 100)
 
+def MainMenu(name):
+    choice = 0
+    #loop through main menu util user quits
+    while(choice != 4):
+        choice = 0
+        clearScreen()
+        print("Welcome", name, "to Blackjack Game!")
+        print("1. Play Game")
+        print("2. View Leaderboard")
+        print("3. View Your Score")
+        print("4. Quit")
+
+        #loop through for valid input
+        while choice != 1 and choice != 2 and choice != 3 and choice != 4:
+            try:
+                choice = int(input("Please enter a choice."))
+            except:
+                choice = 0
+                continue
+
+        if(choice == 1):
+            #play game
+            clearScreen()
+            newG = Game(name)
+            newG.playGame()
+            dummyvar = input("Press any button to continue.")
+
+        elif(choice == 2):
+            #view leaderboard
+            clearScreen()
+            viewScoreboard()
+            dummyvar = input("Press any button to continue.")
+
+        elif(choice == 3):
+            #view your score
+            clearScreen()
+            viewYourScore()
+            dummyvar = input("Press any button to continue.")
+
+
+def viewScoreboard():
+    print("###Scoreboard###")
+    scoreboard = pd.read_csv('scoreboard.csv')
+    #get scoreboard, group by player name, and sort by wins first, then losses
+    scoreboard = scoreboard.groupby(['name']).sum()
+    scoreboard.sort_values(by=['win', 'loss'], inplace=True, ascending=[False, True])
+    print(scoreboard)
+
+def viewYourScore():
+    print("###Your Score###")
+    #gets the score for only your name
+    scoreboard = pd.read_csv('scoreboard.csv')
+    scoreboard = scoreboard[scoreboard['name'].str.contains(pname)].groupby(['name']).sum()
+    print(scoreboard)
 
 class Game:
 
@@ -28,9 +82,9 @@ class Game:
     Y = dataset.decision.astype(int)
     logreg.fit(X, Y)
 
-    def __init__(self):
+    def __init__(self, name):
         self.Deck = Deck.Deck()
-
+        self.pname = name
         self.playerHand = Hand.Hand()
         self.compHand = Hand.Hand()
 
@@ -65,7 +119,7 @@ class Game:
             #used to make player play automatically
             #playerchoice = self.playerAutoAction()
             if playerchoice == 1:
-                self.playerActionArr.append(['data.csv', 'dataset', self.playerHand.countScore(),
+                self.playerActionArr.append(['data.csv', self.pname, self.playerHand.countScore(),
                                              self.compHand.countScore(), self.playerHand.hasAce, playerchoice])
                 self.playerHand.addCard(self.Deck.drawCard())
                 #check if player loses
@@ -74,13 +128,13 @@ class Game:
                     break
             else:
                 self.playerActionArr.append(
-                    ['data.csv', 'dataset', self.playerHand.countScore(), self.compHand.countScore(),
+                    ['data.csv', self.pname, self.playerHand.countScore(), self.compHand.countScore(),
                      self.playerHand.hasAce, 0])
             #computer decision
             #compChoice = self.computerAction()
             compChoice = self.compAIDecision()
             if compChoice == 1:
-                self.compActionArr.append(['data.csv', 'dataset', self.playerHand.countScore(), self.compHand.countScore
+                self.compActionArr.append(['data.csv', self.pname, self.playerHand.countScore(), self.compHand.countScore
                 (), self.compHand.hasAce, compChoice])
                 self.compMsg = "Computer hit."
                 self.compHand.addCard(self.Deck.drawCard())
@@ -91,7 +145,7 @@ class Game:
                     break
             else:
                 self.compActionArr.append(
-                    ['data.csv', 'dataset', self.playerHand.countScore(),
+                    ['data.csv', self.pname, self.playerHand.countScore(),
                      self.compHand.countScore(), self.compHand.hasAce, 0])
                 self.compMsg = "Computer stayed."
 
@@ -101,7 +155,7 @@ class Game:
                     self.draw = True
                 break
 
-        #determine results
+        #determine results, and based on who wins, adds the winner's actions to the dataset for learning
         if(self.playerWin):
             for x in self.playerActionArr:
                 DataAccess.append_data(x)
@@ -126,7 +180,7 @@ class Game:
 
         #see computer decision
         print(self.logreg.predict(x_new))
-        return(self.logreg.predict(x_new))
+        return(int(self.logreg.predict(x_new)))
 
     def computerAction(self):
         #to be replaced with AI decision tree
@@ -161,7 +215,9 @@ class Game:
         else:
             playerLoss = True
 
-        DataAccess.add_score("jeremy", self.playerWin, playerLoss, self.draw)
+
+        #add result to your personal score
+        DataAccess.add_score(self.pname, self.playerWin, playerLoss, self.draw)
 
         print("Player Score: ", self.playerHand.countScore())
         print("Player Hand: ", self.playerHand.printHand())
@@ -196,9 +252,12 @@ class Game:
 
         return choice
 
-x = Game()
-x.playGame()
 
-#y = pd.read_csv('data.csv').head(3)
-#print(y.computerHasAce)
-#print(y['decision'].sum())
+
+
+####   MAIN #####
+
+pname = input("Please enter your name.")
+MainMenu(pname)
+print("Thank you for playing!")
+
